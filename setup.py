b1f2cfd727f64ddb075e25af4d4afb1a6ce97c7c
kazukazuprogram <cabbage.lettuce.wireless@gmail.com>
2018-12-08 01:14:15 +0900

    package

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python
+#coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(name="selfext",
+	version="0.0.0",
+	description="Generate self-extracting script.",
+	url="https://github.com/kazukazuprogram/selfext",
+	packages=find_packages(),
+)
